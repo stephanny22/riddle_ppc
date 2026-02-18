67d4d06fe0d80bdcb0081bb32acb202cca55cb7c
Diana Guevara <stephannyd.guevara@gmail.com>
2026-02-18 15:47:39 -0500

    Es la primera versión hecha en clase

--- 1. riddle simple/app/src/main/java/com/ud/riddle/MainActivity.kt
+++ 1. riddle simple/app/src/main/java/com/ud/riddle/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.ud.riddle
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,8 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.ud.riddle.ui.theme.RiddleAppTheme
 
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             RiddleAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   calculateNames()
                 }
             }
         }
@@ -31,17 +31,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun calculateNames(){
+    val context = LocalContext.current
+    Text(text="Input a name player")
+    TextField(
+        label = { Text( text= "Label")},
+            value= "",
+            onValueChange = {
+                val toast = Toast.makeText(context, "Name %it", Toast.LENGTH_LONG)
+                toast.show()
+            }
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RiddleAppTheme {
-        Greeting("Android")
-    }
 }
